43e3effbaccbe6dfb5161aebd288e3343fc7dd3a
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-04-25 16:49:05 +0900

    nejiko practice

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class NejikoController : MonoBehaviour
 {
 
+    const int MinLane = -2;
+    const int MaxLane = 2;
+    const float LaneWidth = 1.0f;
+    const int DefaultLife = 3;
+    const float StunDuration = 0.5f;
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
+    int targetLane;
+    int life = DefaultLife;
+    float recoverTime = 0.0f;
 
     public float gravity;
     public float speedZ;
+    public float speedX;
     public float speedJump;
+    public float accelerationZ;
+
+    public int Life()
+    {
+        return life;
+    }
+
+    bool IsStun()
+    {
+        return recoverTime > 0.0f || life <= 0;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -24,26 +44,30 @@ public class NejikoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //接地しているかどうかの判定
-        if(controller.isGround)
+
+        //デバック用
+        if (Input.GetKeyDown("left")) MoveLeft();
+        if (Input.GetKeyDown("right")) MoveRight();
+        if (Input.GetKeyDown("space")) Jump();
+
+        if (IsStun())
         {
-            if(Input.GetAxis("Vertical") > 0.0f)
-            {
-                moveDirection.z = Input.GetAxis("Vertical") * speedZ;
-            }
-            else
-            {
-                moveDirection.z = 0;
-            }
-        //方向の回転
-            transition.Rotate(0,Input.GetAxis("Horizontal") * 3,0);
-        //ジャンプ処理
-            if(Input.GetButton("Jump"))
-            {
-                moveDirection.y = speedJump;
-                animator.SetTrigger("jump");
-            }
+            //動きを止め気絶状態から復帰カウントを進める
+            moveDirection.x = 0.0f;
+            moveDirection.z = 0.0f;
+            recoverTime -= Time.deltaTime;
         }
+        else
+        {
+            //徐々に加速しz方向に常に前進させる
+            float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+            moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+
+            //x方向は目標のポジションまでの差分の割合で速度を計算
+            float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+            moveDirection.x = ratioX * speedX;
+        }
+
 
         //重力分の力をマイフレーム追加
         moveDirection.y -= gravity * Time.deltaTime;
@@ -53,9 +77,49 @@ public class NejikoController : MonoBehaviour
         controller.Move(globalDirection * Time.deltaTime);
 
         //移動後接地してたらy方向の速度はリセっとする
-        if(controller.isGround) moveDirection.y = 0;
+        if (controller.isGrounded) moveDirection.y = 0;
 
         //速度が０以上なら走っているフラグをtrueにする
-        aninmator.SetBool("run",moveDirection.z > 0.0f);
+        animator.SetBool("run", moveDirection.z > 0.0f);
+    }
+
+    public void MoveLeft()
+    {
+        if (IsStun()) return;
+        if (controller.isGrounded && targetLane > MinLane) targetLane--;
+    }
+    public void MoveRight()
+    {
+        if (IsStun()) return;
+        if (controller.isGrounded && targetLane < MaxLane) targetLane++;
+    }
+
+    public void Jump()
+    {
+        if (IsStun()) return;
+        if (controller.isGrounded)
+        {
+            moveDirection.y = speedJump;
+            //ジャンプトリガーを設定
+            animator.SetTrigger("jump");
+        }
+    }
+
+    //CharacterControllerに衝突判定が生じたときの処理
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (IsStun()) return;
+
+        if (hit.gameObject.tag == "Robo")
+        {
+            //ライフを減らして気絶状態に移行
+            life--;
+            recoverTime = StunDuration;
+
+            //ダメージトリガーを設定
+            animator.SetTrigger("damage");
+            //ヒットしたオブジェクトは削除
+            Destroy(hit.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/StageGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+    const int StageChipSize = 30;
+
+    int currentChipIndex;
+
+    public Transform character;
+    public GameObject[] stageChips;
+    public int startChipIndex;
+    public int preInstantiate;
+    public List<GameObject> generatedStageList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初期化処理
+        currentChipIndex = startChipIndex - 1;
+        UpdateStage(preInstantiate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キャラクターの位置から現在のステージチップのインデックスを計算
+        int CharaPositionIndex = (int)(character.position.z / StageChipSize);
+        //次のステージチップに入ったらステージの更新処理を行う
+        if (CharaPositionIndex + preInstantiate > currentChipIndex)
+        {
+            UpdateStage(CharaPositionIndex + preInstantiate);
+        }
+    }
+
+    //指定のindexまでのステージチップを生成して、管理下に置く
+    void UpdateStage(int toChipIndex)
+    {
+        if (toChipIndex <= currentChipIndex) return;
+
+        //指定のステージチップまでを作成
+        for (int i = currentChipIndex + 1; i <= toChipIndex; i++)
+        {
+            GameObject stageObject = GenerateStage(i);
+
+            //生成したステージチップを管理リストに追加
+            generatedStageList.Add(stageObject);
+        }
+
+        //ステージ保持上限になるまで古いステージを削除
+        while (generatedStageList.Count > preInstantiate + 2) DestroyOldestStage();
+
+        currentChipIndex = toChipIndex;
+    }
+
+    //指定のインデックス位置にStageオブジェクトをランダムに生成
+    GameObject GenerateStage(int toChipIndex)
+    {
+        int nextStageChip = Random.Range(0, stageChips.Length);
+
+        GameObject stageObject = (GameObject)Instantiate(
+            stageChips[nextStageChip],
+            new Vector3(0, 0, toChipIndex * StageChipSize),
+            Quaternion.identity
+        );
+
+        return stageObject;
+    }
+    //一番古いステージを削除
+    void DestroyOldestStage()
+    {
+        GameObject oldStage = generatedStageList[0];
+        generatedStageList.RemoveAt(0);
+        Destroy(oldStage);
+    }
+}
