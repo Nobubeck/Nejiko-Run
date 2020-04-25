1f968493956b5e7d7438292b0d1931c6e23defcd
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-04-25 20:02:00 +0900

    nejiko  fix

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スコアを更新
+        int score = CalcScore();
+        scoreText.text = "Score : " + score + "m";
+
+        //ライフパネルを更新
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore()
+    {
+        //ねじ子の走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+    }
+}
