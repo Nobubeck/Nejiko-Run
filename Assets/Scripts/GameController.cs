e6c70e0eea8de37557cfd43abfd38a2e0938050a
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-04-26 17:51:17 +0900

    nejiko complete

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -24,11 +25,35 @@ public class GameController : MonoBehaviour
 
         //ライフパネルを更新
         lifePanel.UpdateLife(nejiko.Life());
+
+        //ねじ子のライフが０になったらゲームオーバー
+        if (nejiko.Life() <= 0)
+        {
+            //これ以降のUpdateは止める
+            enabled = false;
+
+            //ハイスコアを更新
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+
+            //２秒後にReturnToTitleを呼び出す
+            Invoke("ReturnToTitle", 2.0f);
+
+
+        }
     }
 
     int CalcScore()
     {
         //ねじ子の走行距離をスコアとする
         return (int)nejiko.transform.position.z;
+    }
+
+    void ReturnToTitle()
+    {
+        //タイトルシーンに切り替え
+        SceneManager.LoadScene("Title");
     }
 }
